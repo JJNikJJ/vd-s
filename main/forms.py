ecc726f62174bd48225fbb3b872e451d6142cb8c
CpedaMonsterEnergyLover <vova.cpeda@yandex.ru>
2023-09-12 18:29:54 +0600

    фикс

--- main/forms.py
+++ main/forms.py
@@ -5,6 +5,7 @@ from main.models import Checkout
 class CheckoutAdminForm(forms.ModelForm):
     class Meta:
         model = Checkout
+        fields = '__all__'
 
     def close_checkout_action(self):
         print("Order closed")
